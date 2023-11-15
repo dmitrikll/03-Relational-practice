08d1040dd3eb3920dc22743fc09b8721c0c8c168
Dmytro Halytskyi <ds.galytskyi@gmail.com>
2023-11-15 22:18:23 +0200

    add task02

---
+++ src/task02/Main.java
@@ -0,0 +1,33 @@
+package task02;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static String trafficLight;
+
+    public static void main(String[] args) {
+
+        Scanner scannerTraffic = new Scanner(System.in);
+
+        System.out.print("Please, enter the traffic light (red; yellow; green): ");
+        trafficLight = scannerTraffic.nextLine();
+
+        switch (trafficLight) {
+            case "red":
+                System.out.println("Please, stand still.");
+                break;
+            case "yellow":
+                System.out.println("Please, stand by.");
+                break;
+            case "green":
+                System.out.println("GO !!!\n");
+            default:
+                System.out.println("Traffic lights do not work...");
+
+        }
+
+        scannerTraffic.close();
+    }
+
+}
